85fa689492567444d8b3c73d433563582156ba16
b3tst <b3tst@b3tst.b3>
2022-08-15 22:37:30 -0500

    juego pokemon

---
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>¿Quien es este pokémon?</h1>
+
+  <!-- TODO: Picture -->
+  <pokemon-picture v-bind:pokemonId="40" :showPokemon="false" />
+  <!-- TODO: OPCIONES -->
+  <pokemon-opcions :pokemons="pokemonArr" />
+</template>
+
+<script>
+import PokemonPicture from "@/components/PokemonPicture.vue";
+import PokemonOpcions from "@/components/PokemonOpcions.vue";
+import getPokemonOptions from "@/pages/helpers/getPokemonOptions";
+
+// console.log(getPokemonOptions());
+
+export default {
+  components: {
+    PokemonPicture,
+    PokemonOpcions,
+  },
+  data() {
+    return {
+      pokemonArr: [jidasiodaks],
+    };
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions(jidasiodaks);
+      console.log(this.pokemonArr);
+    },
+  },
+  mounted() {
+    this.mixPokemonArray();
+  },
+};
+</script>
+
+<style>
+</style>
