478afe154ff01766fc6d17cede42fc6b4c86cc89
b3tst <b3tst@b3tst.b3>
2022-08-16 02:14:37 -0500

    rutas

--- 05-rutas-ciclo/src/shared/components/NavBar.vue
+++ 05-rutas-ciclo/src/shared/components/NavBar.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    <router-link to="/">Pokemon List</router-link>
+    <!-- <router-link :to="{ name: 'home' }">Pokemon List</router-link>
     <router-link :to="{ name: 'pokemon-id', params: { id: 85 } }"
       >Pokemon por id</router-link
     >
-    <router-link to="/about">About</router-link>
+    <router-link :to="{ name: 'about' }">About</router-link> -->
+    <customLink v-for="link in links" 
+    :key="link.to" :link="link" />
   </div>
 </template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+export default {
+  data() {
+    return {
+      links: [
+        { to: "/home", name: "pokemons" },
+        { to: "/pokemonid/50", name: "por ID" },
+        { to: "/about", name: "About" },
+        { to: "https://google.com", name: "Google" },
+      ],
+    };
+  },
+  components: {
+    CustomLink: defineAsyncComponent(() => import("./CustomLink.vue")),
+  },
+};
+</script>
 
 <style scoped>
 div {
